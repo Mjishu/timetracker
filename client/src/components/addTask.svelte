d84712134f3b83a31d11668c2522b3b76cfca7ab
mjishu <128941980+Mjishu@users.noreply.github.com>
2024-11-02 22:16:48 -0400

    added server setup and added testing support for client

---
+++ client/src/components/addTask.svelte
@@ -0,0 +1,96 @@
+<script>
+	import { isModalOpen } from '../shared';
+	let newTask = $state({
+		task: '',
+		time: '',
+		date: '',
+		type: ''
+	});
+
+	const types = [
+		{
+			id: 1,
+			text: 'Reading'
+		},
+		{
+			id: 2,
+			text: 'Exercise'
+		},
+		{
+			id: 3,
+			text: 'coding'
+		},
+		{
+			id: 4,
+			text: 'Development'
+		}
+	];
+
+	/**
+	 * @param {{ preventDefault: () => void; }} e
+	 */
+	function handleSubmit(e) {
+		e.preventDefault();
+		console.log($state.snapshot(newTask));
+		isModalOpen.set(false);
+	}
+</script>
+
+{#if $isModalOpen}
+	<div class="modal">
+		<form class="modal-content" onsubmit={handleSubmit}>
+			<label for="time">Task</label>
+			<input type="text" bind:value={newTask.task} id="task" />
+
+			<label for="task">Time</label>
+			<input type="text" bind:value={newTask.time} id="time" />
+
+			<label for="date">Date</label>
+			<input type="Date" bind:value={newTask.date} id="date" />
+
+			<select bind:value={newTask.type}>
+				{#each types as type}
+					<option value={type.text}>{type.text}</option>
+				{/each}
+			</select>
+
+			<button onclick={() => isModalOpen.set(false)}>cancel</button>
+			<button type="submit">Add task</button>
+		</form>
+	</div>
+{/if}
+
+<style>
+	.modal {
+		display: block;
+		align-items: center;
+		position: fixed;
+		z-index: 1;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 100%;
+		overflow: auto;
+		background-color: rgb(0, 0, 0);
+		background-color: rgba(0, 0, 0, 0.4);
+	}
+
+	.modal-content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		background: #fefefe;
+		margin: 15% auto;
+		padding: 20px;
+		border: 1px solid #888;
+		width: 25%;
+		height: 20rem;
+		border-radius: 10px;
+		border: none;
+	}
+
+	.modal-content input,
+	.modal-content button {
+		width: fit-content;
+	}
+</style>
---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import AddTask from '../components/addTask.svelte';
+	import { isModalOpen } from '../shared';
+
+	function handleOpenModal() {
+		isModalOpen.set(true);
+	}
+</script>
+
+<button class="add-task-button" onclick={handleOpenModal}>Add Task</button>
+<AddTask />
